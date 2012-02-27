f9997df5be5c4d2a6054575f0a96a1c0d8d83212
MoLice <sf.molice@gmail.com>
2012-02-27 23:19:10 +0800

    @MoLice 完成校园卡，创建订水。

---
+++ 1/oneingdufs/life/forms.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+"""oneingdufs.life.forms 校园生活forms
+
+@author MoLice<sf.molice@gmail.com>
+|- Water 订水
+"""
+
+from django import forms
+# project import
+
+class Water(forms.Form):
+  """订水表单"""
+  # 宿舍
